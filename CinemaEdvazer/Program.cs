d7d50722b449d65697c3f912f67be408709a0599
artoryfagottino <artorufagottino@gmail.com>
2022-01-18 00:12:24 +0300

    first lesson C#,CinemaEdvaizer

---
+++ CinemaEdvazer/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine("Здравствуйте!Эта программа поможет вам выбрать фильм");
+Console.WriteLine("Как вас зовут?");
+string name = Console.ReadLine();
+Console.WriteLine("Здраствуйте, "+ name);
+Console.WriteLine("Фильм какого жанры вы предпочитаете: комедия,ужасы,драмма,фантастика?");
+string genre = Console.ReadLine();
+ Console.WriteLine();
+ 
+ Console.WriteLine("**Есть такие фильмы: ");
+if(genre == "комедия")
+{
+  Console.ForegroundColor = ConsoleColor.DarkYellow;
+  Console.WriteLine("* Операция Ы ");
+  Console.WriteLine("* 1+1 ");
+  Console.WriteLine("* Назад в Будущее");
+}
+
+else if (genre == "фантастика")
+{
+  Console.ForegroundColor = ConsoleColor.DarkYellow;
+  Console.WriteLine("* Черепашки Ниндзя");
+  Console.WriteLine("* Люди Х");
+    
+}
+else if (genre == "ужасы")
+{
+  Console.ForegroundColor = ConsoleColor.DarkYellow;
+  Console.WriteLine("* Оно");
+  Console.WriteLine("* Звонок");
+} 
+else
+{
+   
+    Console.WriteLine("Зеленная миля ");
+    Console.WriteLine("Побег из Шоушенга");
+
+}
+Console.ReadLine();
